85a83c173f827e79cd0b781c7884913be449dd29
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2024-05-13 20:17:09 +0200

    new classes added

---
+++ Aura/Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Jakub Grobelkiewicz Copyright
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
